e02fb95f3cd27b27b6b79be3177edb4a993ddf56
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-06 23:57:49 +0100

    Spring MVC

---
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/Spring08SpringBootMvcControllerApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring08SpringBootMvcControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring08SpringBootMvcControllerApplication.class, args);
+    }
+
+}
