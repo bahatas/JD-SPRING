ab2330df0bbb88f658e09bdfb4e1927f0113d5f5
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-10 19:09:52 +0100

    MVC FORM

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/contoller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/contoller/MentorController.java
@@ -1,20 +1,37 @@
 package com.cybertek.contoller;
 
+import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
     @GetMapping("/register")
-    public String showForm(Model model){
+    public String showForm(Model model) {
 
+        Mentor mentorObj = new Mentor();
+        model.addAttribute("mentor",mentorObj );
+
+        List<String> batchList = Arrays.asList("B7","B8","B9","B10","B11","B12","B13","B14","B15");
+        model.addAttribute("batchList",batchList);
 
 
         return "mentor/mentor-register";
 
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+        System.out.println(mentor.toString());
+        return "mentor/mentor-confirmation";
     }
 }
--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Mentor.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/model/Mentor.java
@@ -10,7 +10,7 @@ import lombok.*;
 @ToString
 public class Mentor {
 
-    private String firstanme;
+    private String firstname;
     private String lastname;
     private String email;
     private String gender;
