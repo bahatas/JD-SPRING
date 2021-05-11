53725974bd4a9c667f875d501371153c2f493c5d
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-11 23:03:57 +0100

    MVC FORM

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/contoller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/contoller/EmployeeController.java
@@ -5,23 +5,21 @@ import com.cybertek.model.Employee;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    @GetMapping("/create")
+    //@GetMapping("/create")
+    @RequestMapping(value = "/create",method = RequestMethod.GET)
     public String createEmployee(Model model){
 
         model.addAttribute("employeeAttribute",new Employee());
 
-        return "/employee/employee-creater";
+        System.out.println("Employee creator");
+        return "employee/employee-creator";
 
 
     }
