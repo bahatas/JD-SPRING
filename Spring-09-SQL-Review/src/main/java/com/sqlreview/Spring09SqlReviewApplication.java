2f11cba19fa7e07fb47b8da12d54faef29ce8100
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-23 19:06:57 +0100

    MVC FORM

---
+++ Spring-09-SQL-Review/src/main/java/com/sqlreview/Spring09SqlReviewApplication.java
@@ -0,0 +1,13 @@
+package com.sqlreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SqlReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09SqlReviewApplication.class, args);
+    }
+
+}
---
+++ Spring-09-SQL-Review/src/test/java/com/sqlreview/Spring09SqlReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SqlReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
