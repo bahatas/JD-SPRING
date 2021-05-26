e7b2c22f4278afafd80f9a486fff42c7290b1b95
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-05-26 23:09:04 +0100

    SQL index

---
+++ Spring-091-ORM-TableCreation_/src/main/java/com/cybertek/entity/Student.java
@@ -0,0 +1,19 @@
+package com.cybertek.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+
+}
---
+++ Spring-091-ORM-TableCreation_/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into student(id,first_name,last_name,email) VALUES  (1,'Mike','Smith','mike@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (2,'Adam','Shaun','Adam@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (3,'Tom','Eddy','Tom@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (4,'brad','ertds','brad@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (5,'Sam','kuhf','kuhf@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (6,'Matt','Brandd','Brandd@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (7,'Mike','Smith','mike@cybertekschool.com');
+insert into student(id,first_name,last_name,email) VALUES  (8,'Mike','Smith','mike@cybertekschool.com');
