a4009cfd1f52e86e8e116e4b4640b1da3320a54c
bahatas <baha@cybetekschool.com>
2021-06-12 09:14:28 +0100

    Spring Security

---
+++ Spring-100-Security-DefaultConfiguration/src/main/java/com/securitydefault/SecurityDefaultApplication.java
@@ -0,0 +1,13 @@
+package com.securitydefault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDefaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDefaultApplication.class, args);
+    }
+
+}
---
+++ Spring-100-Security-DefaultConfiguration/src/test/java/com/securitydefault/SecurityDefaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitydefault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDefaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
