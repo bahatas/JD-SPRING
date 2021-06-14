54186bee3a034366775e87f962c708c80e5379ac
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-14 21:24:47 +0100

    Spring Security Http BasicAuthRoleBase

--- Spring-102-Security-HttpBasic-AuthRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
+++ Spring-102-Security-HttpBasic-AuthRoleBase/src/main/java/com/cybertek/security/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.cybertek.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -56,7 +59,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .and().
+                withUser("baha").password(passwordEncoder().encode("baha123")).roles("USER")
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
+
     }
 
     /**
@@ -83,8 +94,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("index.html").permitAll()
                 .antMatchers("/profile/**").authenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/management/**").hasAnyRole("ADMIN","MAMAGER")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MAMAGER")
                 .and()
                 .httpBasic();
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
