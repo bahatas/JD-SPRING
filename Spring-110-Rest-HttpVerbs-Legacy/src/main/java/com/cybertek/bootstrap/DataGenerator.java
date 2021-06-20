47b3d71ace3560c85b88e7098b33f98b55e862e2
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-20 10:13:09 +0100

    Spring REST-HTTPVerbs-Legacy

---
+++ Spring-110-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -0,0 +1,30 @@
+package com.cybertek.bootstrap;
+
+import com.cybertek.entity.Product;
+import com.cybertek.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+
+public class DataGenerator implements CommandLineRunner {
+
+    public DataGenerator(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    private ProductRepository productRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Product pc = new Product("DELL");
+        Product laptop = new Product("MACBOOK");
+        Product phone = new Product("Iphone");
+        Product tablet = new Product("IPAD");
+
+        productRepository.save(pc);
+        productRepository.save(laptop);
+        productRepository.save(phone);
+        productRepository.save(tablet);
+    }
+
+
+}
