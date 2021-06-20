bd6f9a49be9240c8c6260bca7c314541124fec2b
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-20 17:58:50 +0100

    Spring REST-HTTPVerbs-Preferred

---
+++ Spring-111-Rest-HttpVerbs-Preferrred/src/main/java/com/cybertek/PreferredApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreferredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreferredApplication.class, args);
+    }
+
+}
