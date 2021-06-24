0ce803b4959441c4001f316d7a165d05fb5baa49
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-24 23:46:36 +0100

    Spring Rest API 3.0

--- Spring-113-Rest-Jackson_/src/main/java/com/cybertek/controller/WebFluxController.java
+++ Spring-113-Rest-Jackson_/src/main/java/com/cybertek/controller/WebFluxController.java
@@ -5,9 +5,11 @@ import com.cybertek.entity.MovieCinema;
 import com.cybertek.repository.GenreRepository;
 import com.cybertek.repository.MovieCinemaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class WebFluxController {
@@ -27,6 +29,12 @@ public class WebFluxController {
     @GetMapping("/flux-movie-cinemas")
     public Flux<MovieCinema> readAllCinemaFlux() {
         return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readbyId(@PathVariable("id") Long id){
+
+       return Mono.just(movieCinemaRepository.findById(id).get());
     }
 
 
