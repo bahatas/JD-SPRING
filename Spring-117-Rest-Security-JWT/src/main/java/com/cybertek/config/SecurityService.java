099e09df3a36fe6dcf3741d7f9a081fe6e5d24c8
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-06-28 22:33:44 +0100

    Spring Rest SCURITY JWT

---
+++ Spring-117-Rest-Security-JWT/src/main/java/com/cybertek/config/SecurityService.java
@@ -0,0 +1,47 @@
+package com.cybertek.config;
+
+import com.cybertek.entity.User;
+import com.cybertek.service.UserService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SecurityService implements UserDetailsService {
+
+    private UserService userService;
+
+    public SecurityService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        User foundUser = loadUser(s);
+        if(foundUser == null){
+            throw new UsernameNotFoundException("user not found! " + s);
+        }
+
+        return new org.springframework.security.core.userdetails.User(foundUser.getUsername(), foundUser.getPassword(), listAuthorities(foundUser));
+    }
+
+    public User loadUser(String value){
+        boolean isEmail = value.contains("@");
+        return isEmail ? userService.readByEmail(value) : userService.readByUsername(value);
+    }
+
+    private List<GrantedAuthority> listAuthorities(User user){
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+        grantedAuthorityList.add(new SimpleGrantedAuthority(user.getRole().name()));
+        return grantedAuthorityList;
+    }
+
+
+}
---
+++ Spring-117-Rest-Security-JWT/src/main/java/com/cybertek/util/JWTUtil.java
@@ -0,0 +1,11 @@
+package com.cybertek.util;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTUtil {
+
+
+
+}
