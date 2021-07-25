f9fa0533666ab5f82b0b2ffe6ecd6617e823f46d
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-07-25 23:29:46 +0100

    Cinema Tickets Database Lab

---
+++ Spring-090-CinemaLab/src/main/java/com/cinema/entity/MovieCinema.java
@@ -0,0 +1,28 @@
+package com.cinema.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class MovieCinema extends BaseEntity {
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)// fetch type default eager we correct ti it lazy
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cinema_id")
+    private Cinema cinema;
+}
