c1cb77bff5f87ff5cd8d8336ee77ecf19f9457f5
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-09-21 00:08:16 +0100

    Unit Test Junit5

---
+++ Spring-130-UnitTesting-Junit_5/src/main/java/com/cybertek/test/Calculator.java
@@ -0,0 +1,9 @@
+package com.cybertek.test;
+
+public class Calculator {
+
+    public static int add(int num1, int num2){
+
+        return num1+num2;
+    }
+}
---
+++ Spring-130-UnitTesting-Junit_5/src/test/java/com/cybertek/test/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.cybertek.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+}
