058808a9e6ee0bf30a213619548d952430f7921f
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2022-06-21 10:34:14 +0100

    Add ConsumingAPI-FeignClient

---
+++ Spring-114-1-Rest-ConsumingAPI/src/main/java/com/cydeo/spring1141restconsumingapi/Spring1141RestConsumingApiApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring1141restconsumingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1141RestConsumingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring1141RestConsumingApiApplication.class, args);
+    }
+
+}
---
+++ Spring-114-1-Rest-ConsumingAPI/src/test/java/com/cydeo/spring1141restconsumingapi/Spring1141RestConsumingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring1141restconsumingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1141RestConsumingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
